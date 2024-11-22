5acb76eb5fa835fd0ee7aeb25e22311f34f500ec
GustavJones <gustavjones3@gmail.com>
2024-11-22 19:51:17 +0000

    Project Setup

--- SetupAdvancedWebserver.sh
+++ SetupAdvancedWebserver.sh
@@ -10,4 +10,3 @@ AdvancedWebserver-Configure-Tool --set-data-dir=server /css/ folder_io css/ text
 AdvancedWebserver-Configure-Tool --set-data-dir=server /font.ttf file_io font.ttf font/ttf
 AdvancedWebserver-Configure-Tool --set-data-dir=server /icon.ico file_io images/icon.ico images/x-icon
 AdvancedWebserver-Configure-Tool --set-data-dir=server /library file_io library.html text/html 
-AdvancedWebserver-Configure-Tool --set-data-dir=server /library/ folder_io library/ text/html 
